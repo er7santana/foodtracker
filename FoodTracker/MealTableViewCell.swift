5e08870fda46ad6f177b5f7180d55c522aa81ddf
elsanta <er7.santana@gmail.com>
2019-07-05 19:24:32 -0300

    Meal List Scene added

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by TMAN on 05/07/19.
+//  Copyright © 2019 Shaft Corporation. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
