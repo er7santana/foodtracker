7f80e0273e324686ce0f9317b47c198cf812ed0c
Rodrigo Sant Ana <er.7santana@gmail.com>
2019-07-08 17:28:02 -0300

    feature - cancel editions

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -40,7 +40,18 @@ class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerCo
     
     //MARK: - Navigation
     @IBAction func cancel(_ sender: UIBarButtonItem) {
-        dismiss(animated: true, completion: nil)
+        
+        let isPresentingInAddMealMode = presentingViewController is UINavigationController
+        
+        if isPresentingInAddMealMode {
+            dismiss(animated: true, completion: nil)
+        }
+        else if let owningNavigationController = navigationController {
+            owningNavigationController.popViewController(animated: true)
+        }
+        else {
+            fatalError("The MealViewController is not inside a navigation controller.")
+        }
     }
     
     
