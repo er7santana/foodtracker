f6b737253a6778ede9df75fbf6c18eae17125c2b
Rodrigo Sant Ana <er.7santana@gmail.com>
2019-07-27 18:54:08 -0300

    Circle image in table

---
+++ FoodTracker/UIImageViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  UIImageViewExtension.swift
+//  FoodTracker
+//
+//  Created by Rodrigo Sant Ana on 27/07/19.
+//  Copyright © 2019 Shaft Corporation. All rights reserved.
+//
+
+import Foundation
